09ea37a6fcba1d63bf244c2f04b72dee01d8d235
veryyoung <codingyoung@gmail.com>
2016-05-12 13:29:27 +0800

    Init project

---
+++ app/src/main/java/me/veryyoung/dingding/unrecallled/Main.java
@@ -0,0 +1,42 @@
+package me.veryyoung.dingding.unrecallled;
+
+import android.util.Log;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+
+public class Main implements IXposedHookLoadPackage {
+
+    private static final String DINGDING_PACKAGE_NAME = "com.alibaba.android.rimet";
+
+    private static final String MAP_CLASS_NAME = "com.alibaba.wukong.im.message.MessageImpl";
+    private static final String MAP_FUNCTION_NAME = "recallStatus";
+
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+
+
+        if (lpparam.packageName.equals(DINGDING_PACKAGE_NAME)) {
+
+            XposedBridge.log("Dingding: " + lpparam.appInfo.packageName);
+
+            //map
+            findAndHookMethod(MAP_CLASS_NAME, lpparam.classLoader, MAP_FUNCTION_NAME, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    int recallStatus = (int) param.getResult();
+                    if (recallStatus == 1) {
+                        XposedBridge.log("Ding: recalled massage");
+                    }
+                    param.setResult(0);
+                }
+            });
+        }
+    }
+}
+
